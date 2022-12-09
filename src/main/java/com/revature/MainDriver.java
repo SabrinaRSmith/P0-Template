3ab37dcc1384c551cb54cb30efd1804ccba87ccb
SabrinaRSmith <sabrina813@revature.net>
2022-12-09 15:30:28 -0600

    added logback

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,13 +1,23 @@
 package com.revature;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.revature.controller.RequestMapping;
 
 import io.javalin.Javalin;
 
 public class MainDriver {
 
+	//My loggger
+	public static Logger logger = LoggerFactory.getLogger(MainDriver.class);
+
 	public static void main(String[] args) {
+
+		//test logger
+		logger.info("starting");
+
 		Javalin app = Javalin.create(confg ->{
 			confg.plugins.enableDevLogging();
 		});
