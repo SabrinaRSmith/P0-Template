aeb083056875acdee840c5709e43493d3548fcbc
SabrinaRSmith <sabrina813@revature.net>
2022-12-12 14:21:10 -0600

    wrote createUser method

--- src/main/java/com/revature/repository/UserDao.java
+++ src/main/java/com/revature/repository/UserDao.java
@@ -1,7 +1,14 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.revature.models.User;
 import com.revature.models.UsernamePasswordAuthentication;
+import com.revature.utilities.ConnectionUtil;
 
 public class UserDao {
     
@@ -10,7 +17,32 @@ public class UserDao {
     }
 
     public User createUser(UsernamePasswordAuthentication registerRequest){
-        return null;
+        try(Connection connection = ConnectionUtil.createConnection()){
+            String sql = "insert into users values (default,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, registerRequest.getUsername());
+            ps.setString(2, registerRequest.getPassword());
+            ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
+            User newUser = new User();
+            rs.next();
+            int newId = rs.getInt("id");
+            newUser.setId(newId);
+            newUser.setUsername(registerRequest.getUsername());
+            newUser.setPassword(registerRequest.getPassword());
+            return newUser;
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+            return new User();
+        }
+    }
+
+    public static void main(String[] args) {
+        UserDao dao = new UserDao();
+        UsernamePasswordAuthentication newUser = new UsernamePasswordAuthentication();
+        newUser.setUsername("lomback");
+        newUser.setPassword("I want Spring");
+        System.out.println(dao.createUser(newUser).getId());
     }
 
 }
