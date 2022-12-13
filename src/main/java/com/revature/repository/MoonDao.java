5d30d4f32e93bcbec2b0c945ef81a05da1843910
SabrinaRSmith <sabrina813@revature.net>
2022-12-13 11:32:18 -0600

    added createMoon

--- src/main/java/com/revature/repository/MoonDao.java
+++ src/main/java/com/revature/repository/MoonDao.java
@@ -1,8 +1,14 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import com.revature.models.Moon;
+import com.revature.utilities.ConnectionUtil;
 
 public class MoonDao {
     
@@ -22,8 +28,24 @@ public class MoonDao {
 	}
 
 	public Moon createMoon(String username, Moon m) {
-		// TODO Auto-generated method stub
-		return null;
+		try (Connection connection = ConnectionUtil.createConnection()) {
+			String sql = "insert into moons values (default, ?, ?)";
+			PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1,m.getName());
+			ps.setInt(2, m.getMyPlanetId());
+			ps.execute();
+			ResultSet rs = ps.getGeneratedKeys();
+			Moon newMoon = new Moon();
+			rs.next();
+			int newID = rs.getInt(1);
+			newMoon.setId(newID);
+			newMoon.setName(m.getName());
+			newMoon.setMyPlanetId(m.getMyPlanetId());
+			return newMoon;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());//add logging
+            return new Moon();
+		}
 	}
 
 	public void deleteMoonById(int moonId) {
@@ -33,5 +55,13 @@ public class MoonDao {
 	public List<Moon> getMoonsFromPlanet(int planetId) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void main(String[] args) {
+		MoonDao dao = new MoonDao();
+		Moon newMoon = new Moon();
+		newMoon.setName("Moon");
+		newMoon.setMyPlanetId(1);
+		System.out.println(dao.createMoon("username", newMoon).getId());
 	}
 }
